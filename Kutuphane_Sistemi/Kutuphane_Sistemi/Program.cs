6f628f12ecc423e6b5179304dc7dae229adafa5f
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-02-03 21:50:44 +0300

    Program baslangici degistirildi

--- Kutuphane_Sistemi/Kutuphane_Sistemi/Program.cs
+++ Kutuphane_Sistemi/Kutuphane_Sistemi/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Kutuphane_Sistemi
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UI.Mainpage());
+            Application.Run(new UI.Login());
 
         }
         
