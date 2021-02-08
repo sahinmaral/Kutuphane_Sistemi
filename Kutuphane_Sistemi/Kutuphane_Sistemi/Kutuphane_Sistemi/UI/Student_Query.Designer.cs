44f178e71a575039b584d0e22537aa66bb2cf551
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-02-09 02:12:11 +0300

    Veritabanı , arayüz değiştirildi. Öğrenci sorgulama eklendi.

---
+++ Kutuphane_Sistemi/Kutuphane_Sistemi/Kutuphane_Sistemi/UI/Student_Query.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Kutuphane_Sistemi.UI
+{
+    partial class Student_Query
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_student_n = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_find_student_name = new System.Windows.Forms.Button();
+            this.txt_student_no = new System.Windows.Forms.TextBox();
+            this.btn_find_student_no = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.txt_student_s = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_student_n
+            // 
+            this.txt_student_n.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_student_n.Location = new System.Drawing.Point(192, 23);
+            this.txt_student_n.Name = "txt_student_n";
+            this.txt_student_n.Size = new System.Drawing.Size(144, 26);
+            this.txt_student_n.TabIndex = 69;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(12, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 20);
+            this.label8.TabIndex = 75;
+            this.label8.Text = "Öğrenci adı , soyadı :";
+            // 
+            // btn_find_student_name
+            // 
+            this.btn_find_student_name.BackColor = System.Drawing.Color.Thistle;
+            this.btn_find_student_name.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_find_student_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_find_student_name.Location = new System.Drawing.Point(341, 114);
+            this.btn_find_student_name.Name = "btn_find_student_name";
+            this.btn_find_student_name.Size = new System.Drawing.Size(123, 57);
+            this.btn_find_student_name.TabIndex = 73;
+            this.btn_find_student_name.Text = "Öğrenci adı bul";
+            this.btn_find_student_name.UseVisualStyleBackColor = false;
+            this.btn_find_student_name.Click += new System.EventHandler(this.btn_find_student_name_Click);
+            // 
+            // txt_student_no
+            // 
+            this.txt_student_no.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_student_no.Location = new System.Drawing.Point(192, 61);
+            this.txt_student_no.Name = "txt_student_no";
+            this.txt_student_no.Size = new System.Drawing.Size(278, 26);
+            this.txt_student_no.TabIndex = 71;
+            // 
+            // btn_find_student_no
+            // 
+            this.btn_find_student_no.BackColor = System.Drawing.Color.Thistle;
+            this.btn_find_student_no.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_find_student_no.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_find_student_no.Location = new System.Drawing.Point(192, 114);
+            this.btn_find_student_no.Name = "btn_find_student_no";
+            this.btn_find_student_no.Size = new System.Drawing.Size(123, 57);
+            this.btn_find_student_no.TabIndex = 72;
+            this.btn_find_student_no.Text = "Öğrenci kimlik no bul";
+            this.btn_find_student_no.UseVisualStyleBackColor = false;
+            this.btn_find_student_no.Click += new System.EventHandler(this.btn_find_student_no_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(26, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(140, 20);
+            this.label9.TabIndex = 74;
+            this.label9.Text = "Öğrenci kimlik no : ";
+            // 
+            // btn_print
+            // 
+            this.btn_print.BackColor = System.Drawing.Color.Thistle;
+            this.btn_print.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_print.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_print.Location = new System.Drawing.Point(192, 211);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(277, 57);
+            this.btn_print.TabIndex = 78;
+            this.btn_print.Text = "Sayfalara Verileri Gönder";
+            this.btn_print.UseVisualStyleBackColor = false;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // txt_student_s
+            // 
+            this.txt_student_s.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_student_s.Location = new System.Drawing.Point(341, 23);
+            this.txt_student_s.Name = "txt_student_s";
+            this.txt_student_s.Size = new System.Drawing.Size(128, 26);
+            this.txt_student_s.TabIndex = 70;
+            // 
+            // Student_Query
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(488, 283);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.txt_student_s);
+            this.Controls.Add(this.txt_student_n);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_find_student_name);
+            this.Controls.Add(this.txt_student_no);
+            this.Controls.Add(this.btn_find_student_no);
+            this.Controls.Add(this.label9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Student_Query";
+            this.Text = "Öğrenci Sorgulama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_student_n;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btn_find_student_name;
+        private System.Windows.Forms.TextBox txt_student_no;
+        private System.Windows.Forms.Button btn_find_student_no;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.TextBox txt_student_s;
+    }
+}
---
+++ Kutuphane_Sistemi/Kutuphane_Sistemi/Kutuphane_Sistemi/UI/Student_Query.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kutuphane_Sistemi.UI
+{
+    public partial class Student_Query : Form
+    {
+        public Student_Query()
+        {
+            InitializeComponent();
+        }
+
+        public static string name = "";
+        public static string surname = "";
+        public static string id_no = "";
+
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=kutuphane;Integrated Security=True");
+
+        private void btn_find_student_name_Click(object sender, EventArgs e)
+        {
+            if (txt_student_no.Text == "")
+                MessageBox.Show("Öğrencinin numarasını yazmanız gerekiyor", "Bilgilendirme Ekranı");
+
+            else
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SELECT st_name FROM student where st_tr_id=@st_tr_id", con);
+                com.Parameters.AddWithValue("@st_tr_id", txt_student_no.Text);
+                SqlDataReader read = com.ExecuteReader();
+                while (read.Read())
+                {
+                    txt_student_n.Text = read[0].ToString();
+                }
+                con.Close();
+
+
+                con.Open();
+                SqlCommand com2 = new SqlCommand("SELECT st_surname FROM student where st_tr_id=@st_tr_id", con);
+                com2.Parameters.AddWithValue("@st_tr_id", txt_student_no.Text);
+                SqlDataReader read2 = com2.ExecuteReader();
+                while (read2.Read())
+                {
+                    txt_student_s.Text = read2[0].ToString();
+                }
+                con.Close();
+            }
+ 
+
+
+        }
+        private void btn_find_student_no_Click(object sender, EventArgs e)
+        {
+
+            if (txt_student_n.Text == "" && txt_student_s.Text == "")
+                MessageBox.Show("Öğrencinin adını veya soyadını yazmanız gerekiyor", "Bilgilendirme Ekranı");
+
+            else
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SELECT st_tr_id FROM student where st_name=@st_name and st_surname=@st_surname", con);
+                com.Parameters.AddWithValue("@st_name", txt_student_n.Text);
+                com.Parameters.AddWithValue("@st_surname", txt_student_s.Text);
+                SqlDataReader read = com.ExecuteReader();
+                while (read.Read())
+                {
+                    txt_student_no.Text = read[0].ToString();
+                }
+                con.Close();
+            }
+
+        }
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            name = txt_student_n.Text;
+            surname = txt_student_s.Text;
+            id_no = txt_student_no.Text;       
+        }
+    }
+}
